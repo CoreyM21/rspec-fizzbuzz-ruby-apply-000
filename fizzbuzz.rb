d418a878f852528650fce933196f626e997e2c77
Corey <coreyjmartin2@gmail.com>
2020-05-31 18:13:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int = "15")
-  puts "#{x}"
-  if "x" == x / 3
+  puts "#{int}"
+  if "int" == x % 3
   puts "Fizz"
-elsif "x" == x / 5 
+elsif "int" == x % 5 
   puts "Buzz"
-elsif "x" == x / 3 && x / 5
+elsif "int" == x % 3 && x % 5
   puts "FizzBuzz"
 else 
   puts "nil"
