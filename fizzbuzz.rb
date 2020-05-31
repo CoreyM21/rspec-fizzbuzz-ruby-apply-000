fb9fb71e2def50ae7307e232434b008c80baaff7
Corey <coreyjmartin2@gmail.com>
2020-05-31 17:38:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@ def fizzbuzz (x)
   
 end
 
-if x / 3
+if x / 3 == 3
   puts "Fizz"
-elsif x / 5
+elsif x / 5 == 5 
   puts "Buzz"
-elsif x / 15
+elsif x / 3 = 3 == x / 5 = 5
   puts "FizzBuzz"
 else 
   puts "Nil"
