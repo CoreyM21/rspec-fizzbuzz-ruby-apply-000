65924dab66fc39eca78b40193b628c7837a1f909
Corey <coreyjmartin2@gmail.com>
2020-05-31 18:09:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,12 @@
-x = 15
-
-def fizzbuzz(x)
+def fizzbuzz(x = 15)
   puts "#{x}"
 end
 
-if "x" == "x" / 3
+if "x" == x / 3
   puts "Fizz"
-elsif "x" == "x" / 5 
+elsif "x" == x / 5 
   puts "Buzz"
-elsif "x" == "x" / 3 && "x" / 5
+elsif "x" == x / 3 && x / 5
   puts "FizzBuzz"
 else 
   puts "nil"
