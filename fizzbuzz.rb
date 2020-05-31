aac8f6571500568dac7c42757b0d6026d57b95bd
Corey <coreyjmartin2@gmail.com>
2020-05-31 18:06:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 x = 15
 
 def fizzbuzz(x)
-  puts "X"
+  puts "#{x}"
 end
 
 if x == x / 3
@@ -14,10 +14,3 @@ else
   puts "nil"
 end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
