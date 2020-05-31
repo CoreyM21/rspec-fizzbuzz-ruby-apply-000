5859065ccd6669a7007c21df5bf6fc92d728331c
Corey <coreyjmartin2@gmail.com>
2020-05-31 18:05:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -14,4 +14,10 @@ else
   puts "nil"
 end
 
-def display_board( )
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
